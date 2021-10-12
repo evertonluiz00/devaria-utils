476d539dc1bdf079c1e987faa70b92063ef4f2b4
Everton Christmann <evertonluiz00@hotmail.com>
2021-10-12 14:43:56 -0300

    Create ApiModel project

---
+++ Devaria.Utils/ApiModel/Controllers/UsuarioController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiModel.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult BuscarUsuario()
+        {
+            var usuario = new Usuario()
+            {
+                Nome = "Juca",
+                Email = "juca@gmail.com",
+                Senha = "123456"
+            };
+
+            return Ok(usuario);
+        }
+
+        [HttpGet("listar")]
+        public IActionResult ListarUsuarios()
+        {
+            string[] listaNomes = { "Rafael", "Daniel", "Kaique", "Adriano", "Filipe" };
+            return Ok(listaNomes);
+        }
+    }
+}
